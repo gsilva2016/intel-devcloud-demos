194bdbfcf5a3e6e60ae7a79dd1095a45bfd0dbd5
gsilva2016 <gabriel.l.silva@intel.com>
2023-12-15 09:47:43 -0700

    working dual and file/rtsp

--- Dockerfile
+++ Dockerfile
@@ -8,47 +8,32 @@
 # Remove -dpcpp for not dpcpp image
 # For more info refer to: https://hub.docker.com/r/intel/dlstreamer/tags
 ARG BASE_IMAGE=intel/dlstreamer:2023.0.0-ubuntu22-gpu682-dpcpp
-
 FROM $BASE_IMAGE as release
 
 USER root
 ENV DEBIAN_FRONTEND=noninteractive
 SHELL ["/bin/bash", "-c"]
+
 WORKDIR /
 
-# Install dependencies for compiling OpenCV 4.7.0, GStreamer, etc..
+# Install dependencies
 ARG BUILD_DEPENDENCIES="cmake build-essential git-gui python3 python3-pip clang wget curl vim"
 RUN apt -y update && \
     apt install -y ${BUILD_DEPENDENCIES} && \
     rm -rf /var/lib/apt/lists/* && rm -rf /tmp/*
 
-# Install Discrete GPU drivers for Arc. Enables media FF hardware.
-# Note replace arc with flex or max using DGPU_TYPE below
-#ARG DGPU_TYPE="arc"
-#RUN apt update -y; apt install -y wget curl vim gpg-agent; wget -qO - https://repositories.intel.com/graphics/intel-graphics.key | gpg --dearmor --output /usr/share/keyrings/intel-graphics.gpg; echo "deb [arch=amd64,i386 signed-by=/usr/share/keyrings/intel-graphics.gpg] https://repositories.intel.com/graphics/ubuntu jammy $DGPU_TYPE" | tee /etc/apt/sources.list.d/intel-gpu-jammy.list; apt update -y; DEBIAN_FRONTEND=noninteractive apt-get install -y \
-#intel-opencl-icd intel-level-zero-gpu level-zero intel-media-va-driver-non-free libmfx1 libmfxgen1 libvpl2 libegl-mesa0 libegl1-mesa libegl1-mesa-dev libgbm1 libgl1-mesa-dev libgl1-mesa-dri libglapi-mesa libgles2-mesa-dev libglx-mesa0 libigdgmm12 libxatracker2 mesa-va-drivers mesa-vdpau-drivers mesa-vulkan-drivers va-driver-all vainfo libcanberra-gtk-module
-
-# Install OneVPL CPU & Intel GPU hardware accelerated media
-
-# Copy reference sources
-#WORKDIR /app
-RUN ls -l; git clone https://github.com/gsilva2016/vision-self-checkout.git
+# Copy Vision-Checkout reference sources
+RUN git clone https://github.com/gsilva2016/vision-self-checkout.git
 RUN cd vision-self-checkout; git checkout intel-devcloud;
-#RUN cp -R vision-self-checkout/configs/gst-ovms /app/gst-ovms; \
-#cp vision-self-checkout/get-media-codec.sh /app/gst-ovms; \
-#cp vision-self-checkout/configs/gst-ovms/launch-pipeline.sh /app/gst-ovms; \
-#cp -R vision-self-checkout/download_models/ /tmp
 
 RUN mkdir -p /app/dlstreamer/models
-COPY yolo_efficientnet.sh /app/dlstreamer
+COPY yolo_efficientnet*.sh /app/dlstreamer
 RUN cp -R vision-self-checkout/download_models/ /tmp
 RUN cd /tmp/download_models/; ./modelDownload.sh --refresh; cp -R /tmp/configs/dlstreamer/models/2022/* /app/dlstreamer/models/
 
-#RUN cd /app/gst-ovms/pipelines; chmod +x build-demos.sh; ./build-demos.sh
-
-#USER ovms
-
-#ENV LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/:/usr/local/lib/x86_64-linux-gnu/:/usr/local/lib:/ovms/lib:$LD_LIBRARY_PATH
 WORKDIR /app/dlstreamer
+
+# Sample videos go here
+RUN wget -O ./sample-video.mp4 https://www.pexels.com/video/4465029/download/
 
 ENTRYPOINT ["/bin/bash", "-c"]
--- yolo_efficientnet.sh
+++ yolo_efficientnet.sh
@@ -5,86 +5,36 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-
-pre_process=""
-INPUTSRC="rtsp://127.0.0.1:8554"
-# INPUTSRC="file:video.mp4"
-# INPUTSRC="/dev/video0"
-
-INPUT_DEPAY="rtph264depay"
-NSTREAMS=1
-
-if grep -q "rtsp" <<< "$INPUTSRC"; then
-	# rtsp
-	inputsrc=$INPUTSRC" ! $INPUT_DEPAY "
-	INPUTSRC_TYPE="RTSP"
-	pre_process="pre-process-backend=vaapi-surface-sharing pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1"
-
-elif grep -q "file" <<< "$INPUTSRC"; then
-	# filesrc	
-	arrfilesrc=(${INPUTSRC//:/ })
-	# use vids since container maps a volume to this location based on sample-media folder
-	# TODO: need to pass demux/codec info
-	inputsrc="filesrc location=vids/"${arrfilesrc[1]}" ! qtdemux ! h264parse "
-	INPUTSRC_TYPE="FILE"
-	decode_type="vaapidecodebin"
-	pre_process="pre-process-backend=vaapi-surface-sharing pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1"
-
-elif grep -q "video" <<< "$INPUTSRC"; then
-	# v4l2src /dev/video*
-	# TODO need to pass stream info
-	inputsrc="v4l2src device="$INPUTSRC
-	INPUTSRC_TYPE="USB"
-	TARGET_USB_DEVICE="--device=$INPUTSRC"
-	decode_type="videoconvert ! video/x-raw,format=BGR"
-	pre_process=""
-
-# else
-# 	# rs-serial realsenssrc
-# 	# TODO need to pass depthalign info
-# 	inputsrc="realsensesrc cam-serial-number="$INPUTSRC" stream-type=0 align=0 imu_on=false"
-#     # add realsense color related properties if any
-# 	if [ "$COLOR_WIDTH" != 0 ]; then
-# 		inputsrc=$inputsrc" color-width="$COLOR_WIDTH
-# 	fi
-# 	if [ "$COLOR_HEIGHT" != 0 ]; then
-# 		inputsrc=$inputsrc" color-height="$COLOR_HEIGHT
-# 	fi
-# 	if [ "$COLOR_FRAMERATE" != 0 ]; then
-# 		inputsrc=$inputsrc" color-framerate="$COLOR_FRAMERATE
-# 	fi
-# 	INPUTSRC_TYPE="REALSENSE"
-# 	decode_type="decodebin ! videoconvert ! video/x-raw,format=BGR"
-# 	pre_process=""
-# 	cameras=`ls /dev/vid* | while read line; do echo "--device=$line"; done`
-# 	TARGET_GPU_DEVICE=$TARGET_GPU_DEVICE" "$cameras	
-fi
-
-# if [ "$INPUTSRC_TYPE" == "REALSENSE" ]; then
-#     decode_pp="! videoconvert ! video/x-raw,format=BGR"
-# 	# TODO: update with vaapipostproc when MJPEG codec is supported.
-#     echo "Not supported until D436 with MJPEG." > /app/dlstreamer/results/pipeline$cid_count.log
-#     exit 0
-# fi
-
 cid_count=0
+
+# change pre_process="" if using USB or RealSense cameras
+pre_process="pre-process-backend=vaapi-surface-sharing pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1"
+
+# User configured parameters
+INPUTSRC="filesrc location=sample-video.mp4 ! qtdemux ! h264parse "
+#INPUTSRC="rtsp://127.0.0.1:8554/camera_0 ! rtph264depay "
+#INPUTSRC="v4l2src device=/dev/video0 ! videoconvert ! video/x-raw,format=BGR "
+
+DET_MODEL="models/yolov5s/1/FP32-INT8/yolov5s.xml"
+DET_MODEL_PROC="models/yolov5s/1/yolov5s.json"
+RECOG_MODEL="models/efficientnet-b0/1/FP16-INT8/efficientnet-b0.xml"
+RECOG_MODEL_PROC="models/efficientnet-b0/1/efficientnet-b0.json"
+RECOG_LABELS="models/efficientnet-b0/1/imagenet_2012.txt"
 
 if [ "1" == "$LOW_POWER" ]
 then
 	echo "Enabled GPU based low power pipeline "
-	gst-launch-1.0 $inputsrc ! vaapidecodebin ! gvadetect model-instance-id=odmodel name=detection model=models/yolov5s/1/FP32-INT8/yolov5s.xml model-proc=models/yolov5s/1/yolov5s.json threshold=.5 device=GPU $pre_process ! gvatrack name=tracking tracking-type=zero-term-imageless ! queue max-size-bytes=0 max-size-buffers=0 max-size-time=0 ! gvaclassify model-instance-id=clasifier labels=models/efficientnet-b0/1/imagenet_2012.txt model=models/efficientnet-b0/1/FP16-INT8/efficientnet-b0.xml model-proc=models/efficientnet-b0/1/efficientnet-b0.json device=GPU inference-region=roi-list name=classification $pre_process ! gvametaconvert name=metaconvert add-empty-results=true ! gvametapublish name=destination file-format=2 file-path=/app/dlstreamer/results/r$cid_count.jsonl ! fpsdisplaysink video-sink=fakesink sync=true --verbose 2>&1 | tee >/app/dlstreamer/results/gst-launch_core_$cid_count.log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /app/dlstreamer/results/pipeline$cid_count.log)
+	gst-launch-1.0 $INPUTSRC ! vaapidecodebin ! gvadetect model-instance-id=odmodel name=detection model=$DET_MODEL model-proc=$DET_MODEL_PROC threshold=.5 device=GPU $pre_process ! gvaclassify model-instance-id=clasifier labels=$RECOG_LABELS model=$RECOG_MODEL model-proc=$RECOG_MODEL_PROC device=GPU inference-region=roi-list name=classification $pre_process ! gvametaconvert name=metaconvert add-empty-results=true ! gvametapublish name=destination file-format=2 file-path=/app/dlstreamer/results/r$cid_count.jsonl ! fpsdisplaysink video-sink=fakesink sync=false --verbose 2>&1 | tee >/app/dlstreamer/results/gst-launch_core_$cid_count.log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /app/dlstreamer/results/pipeline$cid_count.log)
 elif [ "$RENDER_MODE" == "1" ]
 then
 	echo "Launching rendered pipeline"
-	gst-launch-1.0 $inputsrc ! vaapidecodebin ! gvadetect model-instance-id=odmodel name=detection model=models/yolov5s/1/FP32-INT8/yolov5s.xml model-proc=models/yolov5s/1/yolov5s.json threshold=.5 device=GPU $pre_process ! queue max-size-bytes=0 max-size-buffers=0 max-size-time=0 ! gvaclassify model-instance-id=clasifier labels=models/efficientnet-b0/1/imagenet_2012.txt model=models/efficientnet-b0/1/FP16-INT8/efficientnet-b0.xml model-proc=models/efficientnet-b0/1/efficientnet-b0.json device=GPU inference-region=roi-list name=classification $pre_process ! gvametaconvert name=metaconvert add-empty-results=true ! vaapipostproc ! gvawatermark ! videoconvert ! fpsdisplaysink video-sink=autovideosink sync=false --verbose 2>&1 | tee >/app/dlstreamer/results/gst-launch_core_$cid_count.log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /app/dlstreamer/results/pipeline$cid_count.log)
-
+	gst-launch-1.0 $INPUTSRC ! vaapidecodebin ! gvadetect model-instance-id=odmodel name=detection model=$DET_MODEL model-proc=$DET_MODEL_PROC threshold=.5 device=GPU $pre_process ! gvaclassify model-instance-id=clasifier labels=$RECOG_LABELS model=$RECOG_MODEL model-proc=$RECOG_MODEL_PROC device=GPU inference-region=roi-list name=classification $pre_process ! gvametaconvert name=metaconvert add-empty-results=true ! vaapipostproc ! gvawatermark ! videoconvert ! fpsdisplaysink video-sink=autovideosink sync=false --verbose 2>&1 | tee >/app/dlstreamer/results/gst-launch_core_$cid_count.log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /app/dlstreamer/results/pipeline$cid_count.log)
 elif [ "$CPU_ONLY" == "1" ] 
 then
 	echo "Enabled CPU inference pipeline only"
-	gst-launch-1.0 $inputsrc ! decodebin force-sw-decoders=1 ! gvadetect model-instance-id=odmodel name=detection model=models/yolov5s/1/FP32-INT8/yolov5s.xml model-proc=models/yolov5s/1/yolov5s.json threshold=.5 device=CPU ! gvatrack name=tracking tracking-type=zero-term-imageless ! queue max-size-bytes=0 max-size-buffers=0 max-size-time=0 ! gvaclassify model-instance-id=clasifier labels=models/efficientnet-b0/1/imagenet_2012.txt model=models/efficientnet-b0/1/FP16-INT8/efficientnet-b0.xml model-proc=models/efficientnet-b0/1/efficientnet-b0.json device=CPU inference-region=roi-list name=classification ! gvametaconvert name=metaconvert add-empty-results=true ! gvametapublish name=destination file-format=2 file-path=/app/dlstreamer/results/r$cid_count.jsonl ! fpsdisplaysink video-sink=fakesink sync=true --verbose 2>&1 | tee >/app/dlstreamer/results/gst-launch_core_$cid_count.log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /app/dlstreamer/results/pipeline$cid_count.log)
-
+	gst-launch-1.0 $INPUTSRC ! decodebin force-sw-decoders=1 ! gvadetect model-instance-id=odmodel name=detection model=$DET_MODEL model-proc=$DET_MODEL_PROC threshold=.5 device=CPU ! gvaclassify model-instance-id=clasifier labels=$RECOG_LABELS model=$RECOG_MODEL model-proc=$RECOG_MODEL_PROC device=CPU inference-region=roi-list name=classification ! gvametaconvert name=metaconvert add-empty-results=true ! gvametapublish name=destination file-format=2 file-path=/app/dlstreamer/results/r$cid_count.jsonl ! fpsdisplaysink video-sink=fakesink sync=false --verbose 2>&1 | tee >/app/dlstreamer/results/gst-launch_core_$cid_count.log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /app/dlstreamer/results/pipeline$cid_count.log)
 else
 	echo "Enabled CPU+iGPU pipeline"
-	gst-launch-1.0 $inputsrc ! vaapidecodebin ! gvadetect model-instance-id=odmodel name=detection model=models/yolov5s/1/FP32-INT8/yolov5s.xml model-proc=models/yolov5s/1/yolov5s.json threshold=.5 device=MULTI:GPU,CPU ! gvatrack name=tracking tracking-type=zero-term-imageless ! queue max-size-bytes=0 max-size-buffers=0 max-size-time=0 ! gvaclassify model-instance-id=clasifier labels=models/efficientnet-b0/1/imagenet_2012.txt model=models/efficientnet-b0/1/FP16-INT8/efficientnet-b0.xml model-proc=models/efficientnet-b0/1/efficientnet-b0.json device=MULTI:GPU,CPU inference-region=roi-list name=classification ! gvametaconvert name=metaconvert add-empty-results=true ! gvametapublish name=destination file-format=2 file-path=/app/dlstreamer/results/r$cid_count.jsonl ! fpsdisplaysink video-sink=fakesink sync=true --verbose 2>&1 | tee >/app/dlstreamer/results/gst-launch_core_$cid_count.log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /app/dlstreamer/results/pipeline$cid_count.log)
+	gst-launch-1.0 $INPUTSRC ! vaapidecodebin ! gvadetect model-instance-id=odmodel name=detection model=$DET_MODEL model-proc=$DET_MODEL_PROC threshold=.5 device=GPU $pre_process ! gvaclassify model-instance-id=clasifier labels=$RECOG_LABELS model=$RECOG_MODEL model-proc=$RECOG_MODEL_PROC device=CPU inference-region=roi-list name=classification ! gvametaconvert name=metaconvert add-empty-results=true ! gvametapublish name=destination file-format=2 file-path=/app/dlstreamer/results/r$cid_count.jsonl ! fpsdisplaysink video-sink=fakesink sync=false --verbose 2>&1 | tee >/app/dlstreamer/results/gst-launch_core_$cid_count.log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /app/dlstreamer/results/pipeline$cid_count.log)
 
 fi
